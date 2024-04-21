e5831f8f842a1cd57b06a3d3303347810cf71326
vishalpatel0 <vishal007918@gmail.com>
2024-04-21 11:02:45 +0530

    meta

--- head.php
+++ head.php
@@ -2,13 +2,17 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="nofollow">
+    <meta name="description" content="To create a world where every child has access to quality education,
+regardless">
+    <meta name="author" content="Educate Ferozepur Welfare Society">
 
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Kind Heart Charity - Free Bootstrap 5.2.2 CSS Template</title>
+    <link rel="icon" type="image/png" href="images/logo.png">
+    <title>
+    Educate Ferozepur Welfare Society
+    </title>
 
     <!-- CSS FILES -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
